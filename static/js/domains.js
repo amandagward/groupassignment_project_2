cfcfd1bedc9212cb7ec7e2e756ab1b699a86ae00
pamcuellas <pamcuellas@gmail.com>
2019-10-09 14:28:58 -0400

    Coordinates for Global areas added to domains.js file.

--- static/js/domains.js
+++ static/js/domains.js
@@ -1,9 +1,48 @@
-areas = ["Africa", "Asia", "Europe", "Latin America", "Northern America", "Oceania"];
+var areas = ["Africa", "Asia", "Europe", "Latin America", "Northern America", "Oceania"];
 
-years = ['1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', 
+var years = ['1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', 
                 '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', 
                 '2008', '2009', '2010', '2011', '2012', '2013'];
 
-development = ["Developing regions","Developed regions","All"]; 
+var development = ["Developing regions","Developed regions","All"]; 
 
-globalareas = ["Global Areas", "Top five countries - Africa", "Top five countries - Asia", "Top five countries - Europe", "Top five countries - Latin America", "Top five countries - Oceania", "Top ten countries in the World"];
+var globalareas = ["Global Areas", "Top five countries - Africa", "Top five countries - Asia", "Top five countries - Europe", "Top five countries - Latin America", "Top five countries - Oceania", "Top ten countries in the World"];
+
+var areasCoordinates = [ 
+    {
+        areaname: "Oceania",
+        lat: 140,
+        lon: -10,
+        value: 87897
+    },
+    {
+        areaname: "Africa",
+        lat: 20,
+        lon: 15,
+        value: 87897
+    },
+    {
+        areaname: "Asia",
+        lat: 100,
+        lon: 30,
+        value: 87897
+    },
+    {
+        areaname: "Northern America",
+        lat: -100,
+        lon: 45,
+        value: 87897
+    },
+    {
+        areaname: "Latin America",
+        lat: -70,
+        lon: -5,
+        value: 87897
+    },
+    {
+        areaname: "Europe",
+        lat: 15.2551,
+        lon: 54.5260,
+        value: 87897
+    }    
+];
