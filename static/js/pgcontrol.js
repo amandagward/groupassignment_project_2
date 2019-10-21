78ac15b45720a9e8f07a76bf988ef34610f7315b
pamcuellas <pamcuellas@gmail.com>
2019-10-21 10:37:46 -0400

    Resize event added to modify text on menu bar when screen resized.

--- static/js/pgcontrol.js
+++ static/js/pgcontrol.js
@@ -1,7 +1,22 @@
+// Force colapse hide when Portfolio menu link is clicked 
+$(".migration").click( function (index) {
+  $(".navbar-collapse").collapse('hide');
+});
+
+/* To detect when window is resized */
+$(window).resize(function() {
+  var icon = "<span class='fas fa-globe-americas fa-fw fa-lg' style='color:#1a4f98'></span>";
+  if ($(document).width()<=820)  {
+    $('.migration').html(icon + " Canada - Immigration");
+  } else {
+    $('.migration').html(icon + " Canada - World Immigration Statistics from 1980 to 2013");
+  }
+});
+
+
 // jQuery to control pages
 $( ".dropdown-item" ).each(function( ) {
     $( this ).click( function (index) {
-      console.log("HEREEEE ", this.textContent);
       if ($.trim(this.textContent) == 'Global Areas') {
         $("#map-container").addClass('hide-this-thing');
         $("#top5countries").addClass('hide-this-thing');
